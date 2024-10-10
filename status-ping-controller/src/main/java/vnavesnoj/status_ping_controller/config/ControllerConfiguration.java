9be1f4b570344996ec65a31b0dbac9c028708ec5
vnavesnoj <vnavesnoj@gmail.com>
2024-10-10 14:20:57 +0300

    Add ControllerConfiguration

---
+++ status-ping-controller/src/main/java/vnavesnoj/status_ping_controller/config/ControllerConfiguration.java
@@ -0,0 +1,15 @@
+package vnavesnoj.status_ping_controller.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author vnavesnoj
+ * @mail vnavesnoj@gmail.com
+ */
+@Configuration
+@ComponentScan("vnavesnoj.status_ping_controller")
+@EnableAutoConfiguration
+public class ControllerConfiguration {
+}
